89e3a8a2073fffb1e1b30849ed9faadbe8e527b1
pc chihab domicile <you@example.com>
2015-10-26 11:50:53 +0100

    26/10/2015 11h49

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,114 +1,66 @@
 #include <cstdlib>
-#include <vector>
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp> 
+#include <SFML/Window.hpp>
 #include "Joueur.hpp"
-#include "Pion.hpp"
-#include "Territoire.hpp"
 #include "Etat.hpp"
-#include "Frontiere.hpp"
-#include "TileMap.hpp"
-#include <fstream>
-#include <vector>
-using namespace std;
+
 using namespace sf;
+using namespace std;
 
-int main ()
 
+int main()
 {
-	
-
-	
-	/*Creation des pays*/
-	
-	
-	
-	
-	// on crée la fenêtre
-    sf::RenderWindow window(sf::VideoMode(1152, 1152), "Tilemap");
-
-    // on définit le niveau à l'aide de numéro de tuiles
-    const int level[] =
+    // création de la fenêtre
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "risk");
+    
+    //créaton de l'arrière plan
+    sf::Texture arriere_plan;
+    if (!arriere_plan.loadFromFile("../res/carte_risk.png"))
     {
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-        0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
-    };
+        cout<<"erreur lors du chargement de l'image"<<endl;// erreur...
+    }
+    sf::Sprite sprite;
+    sprite.setTexture(arriere_plan);
 
-    // on crée la tilemap avec le niveau précédemment défini
-    TileMap map;
-    if (!map.load("tileset.png", sf::Vector2u(32, 32), level, 36, 36))
-        return -1;
+    /*-----INITIALISATION JOUEUR-----*/
 
-    // on fait tourner la boucle principale
+    Joueur joueur_1;
+    Joueur joueur_2;
+    Joueur joueur_3;
+
+    joueur_1.set_couleur(1);
+    joueur_2.set_couleur(2);
+    joueur_3.set_couleur(3);
+
+    /*-----INITIALISATION ETAT DE JEU----*/
+
+    Etat etat_de_jeu;
+    etat_de_jeu.set_nombre_joueur(3);
+    
+
+    // on fait tourner le programme tant que la fenêtre n'a pas été fermée
     while (window.isOpen())
     {
-        // on gère les évènements
+        // on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed)
+            // fermeture de la fenêtre lorsque l'utilisateur le souhaite
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        // on dessine le niveau
-        window.clear();
-        window.draw(map);
+        // effacement de la fenêtre en noir
+        window.clear(sf::Color::Black);
+
+        // c'est ici qu'on dessine tout
+        // On dessine l'arrière plan
+        window.draw(sprite);
+
+        // fin de la frame courante, affichage de tout ce qu'on a dessiné
         window.display();
     }
 
     return 0;
 }
-
-
-
-
-
-
-	/*Création de trois joueurs*/
-	
-	//joueur_1 = new Joueur;
-
-	
-	/*Creation d'une fenetre de rendu*/
-	/*
-	sf::RenderWindow window(sf::VideoMode(1150, 1150), "RISK");
-	while (window.isOpen())	
-	{
-		Image fond;
-		Texture texture;
-		if (!texture.loadFromFile("../res/carte_risk.png"))
-		{
-			cout << "\n erreur chargement image\n " << endl;
-		}
-		Sprite sprite;
-		sprite.setTexture(texture);
-		window.clear();
-		window.draw(sprite);
-		window.display();
-	
-	}	
-	
-	return 0;
-	*/
